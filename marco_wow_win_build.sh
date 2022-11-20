c9f5a65f76b55438ce759497d0d78413fb83d24d
hrushik98 <91076764+hrushik98@users.noreply.github.com>
2022-11-20 18:08:07 +0530

    Update marco_wow_win_build.sh
    changed gpt-j-and-gpt-neox20b to neuralsearch-350415

--- marco_wow_win_build.sh
+++ marco_wow_win_build.sh
@@ -1,2 +1,2 @@
-sudo docker image build -t gcr.io/gpt-j-and-gpt-neox20b/marco_wow_win:latest -f deployment.Dockerfile --build-arg MODEL_NAME=marco_wow_win .
-sudo docker push gcr.io/gpt-j-and-gpt-neox20b/marco_wow_win:latest
+sudo docker image build -t gcr.io/neuralsearch-350415/marco_wow_win:latest -f deployment.Dockerfile --build-arg MODEL_NAME=marco_wow_win .
+sudo docker push gcr.io/neuralsearch-350415/marco_wow_win:latest
